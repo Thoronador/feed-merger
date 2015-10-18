39a6f13a784c4a10849bc24f3efe370152a2579f
Thoronador <thoronador@users.sourceforge.net>
2015-10-18 23:40:55 +0200

    add application stub + GUID class for RSS 2.0

---
+++ src/rss2.0/Guid.cpp
@@ -0,0 +1,40 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the feed merger.
+    Copyright (C) 2015 Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "Guid.hpp"
+
+namespace RSS20
+{
+
+GUID::GUID(const std::string& guid, const bool isPermanent)
+: m_guid(guid), m_isPermaLink(isPermanent)
+{ }
+
+const std::string& GUID::get() const
+{
+  return m_guid;
+}
+
+bool GUID::isPermaLink() const
+{
+  return m_isPermaLink && !m_guid.empty();
+}
+
+} //namespace
---
+++ src/rss2.0/Guid.hpp
@@ -0,0 +1,56 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the feed merger.
+    Copyright (C) 2015 Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef RSS20_GUID_HPP
+#define RSS20_GUID_HPP
+
+#include <string>
+
+namespace RSS20
+{
+  /** \brief class to keep GUID information of an RSS 2.0 feed item
+   */
+  class GUID
+  {
+    public:
+      /** default constructor */
+      GUID(const std::string& guid, const bool isPermanent = true);
+
+
+      /** \brief gets the GUID's value
+       *
+       * \return Returns a string representing the GUID's value.
+       */
+      const std::string& get() const;
+
+
+      /** \brief check whether the GUID is a permanent link
+       *
+       * \return Returns true, if the GUID is a permanent link.
+       */
+      bool isPermaLink() const;
+
+    private:
+      std::string m_guid; /**< the GUID's value */
+      bool m_isPermaLink; /**< whether the GUID is a permalink or not */
+  }; //class GUID
+} //namespace
+
+#endif // RSS20_GUID_HPP
