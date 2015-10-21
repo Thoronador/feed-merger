1e6a76a463b9410b2c8b1ed8d0ff9bfbdce70898
Thoronador <thoronador@users.sourceforge.net>
2015-10-22 00:12:20 +0200

    add class for RSS 2.0 category

---
+++ src/rss2.0/Category.cpp
@@ -0,0 +1,42 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the feed merger.
+    Copyright (C) 2015 Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "Category.hpp"
+
+namespace RSS20
+{
+
+Category::Category(const std::string& category, const std::string& domain)
+: m_category(category),
+  m_domain(domain)
+{
+}
+
+const std::string& Category::get() const
+{
+  return m_category;
+}
+
+const std::string& Category::domain() const
+{
+  return m_domain;
+}
+
+} //namespace
---
+++ src/rss2.0/Category.hpp
@@ -0,0 +1,61 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the feed merger.
+    Copyright (C) 2015 Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef RSS20_CATEGORY_HPP
+#define RSS20_CATEGORY_HPP
+
+#include <string>
+
+namespace RSS20
+{
+  /** \brief class that represents an item's category element
+   */
+  class Category
+  {
+    public:
+      /** \brief default constructor
+       *
+       * \param category  the category name
+       * \param domain    value of the domain attribute, optional
+       */
+      Category(const std::string& category, const std::string& domain = "");
+
+
+      /** \brief gets the category's name
+       *
+       * \return Returns a string representing the name.
+       *         Returns an empty string, if no name is set (yet).
+       */
+      const std::string& get() const;
+
+
+      /** \brief gets the category's domain
+       *
+       * \return Returns a string representing the domain.
+       *         Returns an empty string, if no domain is set.
+       */
+      const std::string& domain() const;
+    private:
+      std::string m_category; /**< category name */
+      std::string m_domain; /**< category's domain, if any */
+  }; //class
+} //namespace
+
+#endif // RSS20_CATEGORY_HPP
