e6b6a23e7a95d0283f803962cc87cca4f8081aa2
Thoronador <thoronador@users.sourceforge.net>
2015-10-22 22:04:51 +0200

    add Source class for RSS 2.0

---
+++ src/rss2.0/Source.hpp
@@ -0,0 +1,57 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the feed merger.
+    Copyright (C) 2015 Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef RSS20_SOURCE_HPP
+#define RSS20_SOURCE_HPP
+
+#include <string>
+
+namespace RSS20
+{
+  class Source
+  {
+    public:
+      /** \brief default constructor
+       *
+       * \param _source text of the source element
+       * \param url  the value of the URL attribute
+       */
+      Source(const std::string& _source, const std::string& url);
+
+
+      /** \brief gets the source
+       *
+       * \return Returns the source's name.
+       */
+      const std::string& get() const;
+
+
+      /** \brief gets the source's URL
+       *
+       * \return Returns the value of the url attribute.
+       */
+      const std::string& url() const;
+    private:
+      std::string m_source; /**< source's name */
+      std::string m_url; /**< value of the URL attribute */
+  }; //class
+} //namespace
+
+#endif // RSS20_SOURCE_HPP
